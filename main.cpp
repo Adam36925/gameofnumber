1a3958d767855d79cd84acfa281868a11ca23891
Adam36925 <it-adamlesina@stredniskola.net>
2024-06-07 08:39:23 +0200

    inic

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ctime>
+#include "color.h"
+/* Hadání tajného čísla */
+using namespace std;
+int main(int argc, char *argv[]) {
+   system("chcp 65001>nul"); // Pro podporu UTF-8 v konzoli
+   srand(time(0)); // Inicializace generátoru náhodných čísel s aktuálním časem
+   int tajneCislo;
+   int hracovoCislo;
+   tajneCislo = rand() % 5 + 1; // Náhodné číslo od 1 do 5
+   cout << "Zadej svuj tip na tajne cislo: ";
+   cin >> hracovoCislo;
+   return 0;
+}
